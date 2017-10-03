e23b91b03d9faa126cea62807eff2adb51155ff7
chendelin0595 <chen8526@qq.com>
2017-10-04 01:53:51 +0800

    对接mongodb数据库

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var crypto = require ('crypto');
+var mongoose = require('mongoose');
 var main = require('./index/index');
 var app = express();
 var config = {
@@ -9,6 +10,17 @@ var config = {
 
 app.use(main(config));
 
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/wechat', (err) => {
+	if (err) {
+		console.log('数据库连接失败');
+	} else {
+		console.log('数据库连接成功');
+	}
+})
+
+
 console.log('-------------------开启端口-------------------');
 app.listen(8082);
-console.log('-------------------开启成功-------------------')
+console.log('-------------------开启成功-------------------');
